806d0c0b80fd9aa0d0ac4df0bf0a5482547e6533
eun2051 <genie5518@yu.ac.kr>
2025-04-27 16:17:25 +0900

    Assign1 changed

--- Assign/Assign1.cpp
+++ Assign/Assign1.cpp
@@ -7,80 +7,83 @@ int main() {
 
     while (true) {
         cout << "입력식 : ";
-        getline(cin, s, '\n'); // 문자열 입력
+        getline(cin, s, '\n');
 
         if (s == "end") {
             cout << "종료합니다." << endl;
             break;
         }
 
-        int result = 0;
-        int startIndex = 0;
-        char op = 0;
+        int nums[100]; // 숫자 저장 배열
+        char ops[100]; // 연산자 저장 배열
+        int numCount = 0, opCount = 0;
+        string temp = "";
         bool error = false;
 
-        while (true) {
-            int index = -1;
-            for (char c : "+-*/") {
-                int fIndex = s.find(c, startIndex);
-                if (fIndex != -1 && (index == -1 || fIndex < index)) {
-                    index = fIndex;
-                    op = c;
-                }
-            }
-
-            string part;
-            if (index == -1) {
-                part = s.substr(startIndex);
-                if (part == "") {
-                    cout << "입력식이 잘못되었습니다." << endl;
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+            if (isdigit(c)) {
+                temp += c;
+            } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                if (temp == "") {
                     error = true;
                     break;
                 }
-                int num = stoi(part);
-                if (op == 0) result = num;
-                else if (op == '+') result += num;
-                else if (op == '-') result -= num;
-                else if (op == '*') result *= num;
-                else if (op == '/') {
-                    if (num == 0) {
-                        cout << "0으로 나눌 수 없습니다." << endl;
-                        error = true;
-                        break;
-                    }
-                    result /= num;
-                }
-                break; // 마지막 숫자 처리 후 종료
-            }
-
-            int count = index - startIndex;
-            part = s.substr(startIndex, count);
-
-            if (part == "") {
-                cout << "입력식이 잘못되었습니다." << endl;
+                nums[numCount++] = stoi(temp);
+                ops[opCount++] = c;
+                temp = "";
+            } else {
                 error = true;
                 break;
             }
+        }
 
-            int num = stoi(part);
-            if (op == 0) result = num;
-            else if (op == '+') result += num;
-            else if (op == '-') result -= num;
-            else if (op == '*') result *= num;
-            else if (op == '/') {
-                if (num == 0) {
+        if (temp == "") {
+            error = true;
+        } else {
+            nums[numCount++] = stoi(temp);
+        }
+
+        if (error) {
+            cout << "입력식이 잘못되었습니다." << endl;
+            continue;
+        }
+
+        //*, / 우선 계산
+        for (int i = 0; i < opCount; ) {
+            if (ops[i] == '*' || ops[i] == '/') {
+                if (ops[i] == '/' && nums[i+1] == 0) {
                     cout << "0으로 나눌 수 없습니다." << endl;
                     error = true;
                     break;
                 }
-                result /= num;
+
+                int result = (ops[i] == '*') ? nums[i] * nums[i+1] : nums[i] / nums[i+1];
+                nums[i] = result;
+
+                for (int j = i + 1; j < numCount - 1; j++) {
+                    nums[j] = nums[j + 1];
+                }
+                for (int j = i; j < opCount - 1; j++) {
+                    ops[j] = ops[j + 1];
+                }
+
+                numCount--;
+                opCount--;
+            } else {
+                i++;
             }
-
-            startIndex = index + 1;
         }
 
-        if (!error) {
-            cout << "결과 : " << result << endl;
+        if (error) continue;
+
+        //+, - 계산
+        int result = nums[0];
+        for (int i = 0; i < opCount; i++) {
+            if (ops[i] == '+') result += nums[i + 1];
+            else result -= nums[i + 1];
         }
+
+        cout << "결과 : " << result << endl;
     }
 }
